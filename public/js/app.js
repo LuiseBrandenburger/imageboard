2dbf49f76ac5208f509fc4064b1592a0c270c95e
luise <luisebrandenburger@gmail.com>
2022-01-03 14:06:44 +0100

    basic db setup, needs fixing

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,6 @@
 import * as Vue from './vue.js';
 
 Vue.createApp({
-    /*
-    The fields you specify in the object returned by 
-    the data method will be copied to your Vue app. 
-    Of course, a Vue app is just a Javascript object 
-    and you could add properties to it directly in the 
-    normal way. However, if you add them via the object 
-    returned by the data function of the object you 
-    pass to createApp, they will be reactive properties. 
-    If their values change during the lifetime of your app, 
-    the UI will automatically update to reflect the change. 
-    For this reason, all properties you use in your HTML 
-    should be in the object returned by the data function. 
-    If you don't have a value for the property at the time 
-    you are calling createApp, you should use a placeholder 
-    value such as an empty string or null.
-    */
     // data ist das Daten Objekt, das entsprechend Daten speichert
     data() {
         return {
@@ -50,12 +34,6 @@ Vue.createApp({
                 },
             ],
             greeting: "",
-            /*
-            
-            You can add to your Vue app methods to be called in event handlers (or elsewhere) 
-            by FIXME: adding a methods property to the object you pass to the constructor.
-            
-             */
         };
     },
     methods: {
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const { getImages } = require("../db");
 
 app.use(express.static('./public'));
 
 app.use(express.json());
+
+
+app.get('/imageboard-data', (req, res) => {
+    getImages().then((data) => {
+        console.log("data from get Images", data);
+    });
+});
 
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
