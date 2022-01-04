605870ceb726e848b3cb293d23fa2605b0c38283
luise <luisebrandenburger@gmail.com>
2022-01-04 15:09:35 +0100

    added aws s3 upload for img url

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
 
 Vue.createApp({
     // data ist das Daten Objekt, das entsprechend Daten speichert
@@ -9,6 +9,7 @@ Vue.createApp({
             description: "",
             username: "",
             file: null,
+            spinner: false
         };
     },
     mounted() {
@@ -20,8 +21,32 @@ Vue.createApp({
             });
     },
     methods: {
-        clickHandler: function(e) {
-            console.log("this in function", this);
-        }
+        clickHandler: function () {
+            this.spinner = true;
+            // console.log("this in function", this);
+
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("username", this.username);
+            fd.append("description", this.description);
+            fd.append("file", this.file);
+            fetch('/upload', {
+                method: 'POST',
+                body: fd
+            }).then(res => res.json())
+                .then((result) => {
+                    console.log("result", result);
+                })
+                .catch(console.log());
+            this.title = "";
+            this.description = "";
+            this.username = "";
+            this.file = null;
+
+        },
+        fileSelectHandler: function (e) {
+            console.log("event Object", e);
+            this.file = e.target.files[0];
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const { getImages } = require("./db.js");
+const { getImages } = require("./db");
+const { uploader } = require('./upload');
+const s3 = require("./s3");
+
 
 app.use(express.static('./public'));
-
 app.use(express.json());
 
+
+app.post('/upload', uploader.single('file'), s3.upload, (req, res)=>{
+    console.log("log body: ",req.body);
+    console.log("log file: ", req.file);
+    // res.send('Upload Successfull');
+});
 
 app.get('/get-imageboard-data', (req, res) => {
     getImages().then(({ rows }) => {
