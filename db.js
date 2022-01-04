4a6211a8c93e59fb97d04ae410540a6171734022
luise <luisebrandenburger@gmail.com>
2022-01-04 15:43:21 +0100

    added function to add Images into the db

--- db.js
+++ db.js
@@ -14,3 +14,13 @@ module.exports.getImages = () => {
     const q = `SELECT * FROM images`;
     return db.query(q);
 };
+
+module.exports.addImage = (description, username, title, url) => {
+    const q = `INSERT INTO images (description, username, title, url)
+    VALUES ($1, $2, $3, $4)
+    RETURNING description, username, title, url`;
+
+    const params = [description, username, title, url];
+    return db.query(q, params);
+};
+
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { getImages } = require("./db");
-const { uploader } = require('./upload');
+const { getImages, addImage } = require("./db");
+const { uploader } = require("./upload");
 const s3 = require("./s3");
 
-
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
-
-app.post('/upload', uploader.single('file'), s3.upload, (req, res)=>{
-    console.log("log body: ",req.body);
+app.post("/upload", uploader.single("file"), s3.upload, (req, res) => {
+    console.log("log body: ", req.body);
     console.log("log file: ", req.file);
     // res.send('Upload Successfull');
+    // add file URL to db
+
+    const fileName = req.file.filename;
+    const urlToSaveInDB = `https://s3.amazonaws.com/spicedling/${fileName}`; 
+
+    addImage(
+        req.body.description,
+        req.body.username,
+        req.body.title,
+        urlToSaveInDB
+    ).then(({ rows }) => {
+        console.log("image successfully saved in db");
+        console.log("rows", rows);
+        res.json(rows);
+    }).catch((err)=> {
+        console.log("error adding img to db", err);
+        res.sendStatus(500);
+    });
 });
 
-app.get('/get-imageboard-data', (req, res) => {
+app.get("/get-imageboard-data", (req, res) => {
     getImages().then(({ rows }) => {
         res.json(rows);
     });
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
