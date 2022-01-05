3be7fb6d15bae1fe13f1bf5e0fdd97e5d522496d
luise <luisebrandenburger@gmail.com>
2022-01-05 15:03:59 +0100

    added styling to modal

--- public/js/displayImgModal.js
+++ public/js/displayImgModal.js
@@ -23,12 +23,18 @@ const displayImgModal = {
             this.$emit("close");
         },
     },
-    template: `<div>
-        <img :src="image.url" :alt="image.description" class="img-card">
-        <p class="img-title">{{image.title}}</p>
-        <p>{{image.description}}</p>
-        <button @click="closeModal">Close</button>
-    </div>`,
+    template: `<div class="modal-container">
+            <div class="modal-box">
+            <div class="img-container">
+            <img :src="image.url" :alt="image.description" class="inner-img-modal">
+            </div>
+            <div class="text-container">
+            <h3 class="img-title">{{image.title}}</h3>
+            <p>{{image.description}}</p>
+            </div>
+            <button id="modal-btn" @click="closeModal">Close</button>
+            </div>
+        </div>`,
 };
 
 export default displayImgModal;
